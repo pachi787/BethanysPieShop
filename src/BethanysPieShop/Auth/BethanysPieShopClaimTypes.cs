a830afbd4a2a3ceb1621218421eec51c6f5d6d51
Prashant Goda <pachi787@gmail.com>
2020-01-03 20:02:48 +0530

    Roslynator test

--- src/BethanysPieShop/Auth/BethanysPieShopClaimTypes.cs
+++ src/BethanysPieShop/Auth/BethanysPieShopClaimTypes.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BethanysPieShop.Auth
 {
     public static class BethanysPieShopClaimTypes
     {
         public static List<string> ClaimsList { get; set; } = new List<string> { "Delete Pie", "Add Pie", "Age for ordering" };
+
+        public  static  void TestMethod()
+        {
+            try
+            {
+                ClaimsList = new List<string>();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
     }
 }
