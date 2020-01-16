38b65207dff7ab8cf91602085b0ba948ee18fd0f
Prashant Goda <pachi787@gmail.com>
2020-01-16 17:37:24 +0530

    ps script changes

--- src/BethanysPieShop/CICD/az-Scripts/script.ps1
+++ src/BethanysPieShop/CICD/az-Scripts/script.ps1
@@ -1,3 +1,9 @@
+[CmdletBinding()]
+param (
+    $WorkingDirectory
+)
+$workDirectory = $WorkingDirectory
+
  New-AzResourceGroupDeployment -ResourceGroupName AzureDevOpsResourceGroup  -nameFromTemplate BethanyShop123 `
--TemplateFile "$(System.DefaultWorkingDirectory)\_Prashant_Goda-Azure Web App for ASP.NET-CI\drop\BethanysPieShop\CICD\template.json" `
--TemplateParameterFile "$(System.DefaultWorkingDirectory)\_Prashant_Goda-Azure Web App for ASP.NET-CI\drop\BethanysPieShop\CICD\Parameters.json"
+-TemplateFile "$($workDirectory)\_Prashant_Goda-Azure Web App for ASP.NET-CI\drop\BethanysPieShop\CICD\template.json" `
+-TemplateParameterFile "$($workDirectory)\_Prashant_Goda-Azure Web App for ASP.NET-CI\drop\BethanysPieShop\CICD\Parameters.json"
