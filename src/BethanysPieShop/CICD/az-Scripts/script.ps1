140afac52398d0507f6c393c39b9df071c2e56d8
Prashant Goda <pachi787@gmail.com>
2020-01-16 19:15:23 +0530

    test remove cmlet

--- src/BethanysPieShop/CICD/az-Scripts/script.ps1
+++ src/BethanysPieShop/CICD/az-Scripts/script.ps1
@@ -1,5 +1,7 @@
-[CmdletBinding()]
+
 param ($TemplateFilePath,$TemplateParameterFilePath)
+
+
 
  New-AzResourceGroupDeployment -ResourceGroupName AzureDevOpsResourceGroup  -nameFromTemplate BethanyShop123 `
 -TemplateFile $TemplateFilePath  -TemplateParameterFile  $TemplateParameterFilePath
